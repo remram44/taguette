74e557e58a035c485a24739b6ffdc7fad21960f6
Remi Rampin <remirampin@gmail.com>
2018-09-13 21:45:20 -0400

    Add highlight javascript

--- tagger/static/js/tagger.js
+++ tagger/static/js/tagger.js
@@ -1,5 +1,19 @@
 var output = document.getElementById('output');
 
+function nextElement(node) {
+  while(node && !node.nextSibling) {
+    node = node.parentNode;
+  }
+  if(!node) {
+    return null;
+  }
+  node = node.nextSibling;
+  while(node.firstChild) {
+    node = node.firstChild;
+  }
+  return node;
+}
+
 function describePos(node, offset) {
   while(!node.id) {
     if(node.previousSibling) {
@@ -17,17 +31,15 @@ function locatePos(pos) {
   var id = "doc-item-" + pos.substring(0, split);
   var offset = parseInt(pos.substring(split + 1));
   var node = document.getElementById(id);
+  while(node.firstChild) {
+    node = node.firstChild;
+  }
   while(offset > 0) {
-    if(node.firstChild) {
-      node = node.firstChild;
-    } else if(node.textContent.length >= offset) {
+    if(node.textContent.length >= offset) {
       break;
     } else {
       offset -= node.textContent.length;
-      while(!node.nextSibling) {
-        node = node.parentNode;
-      }
-      node = node.nextSibling;
+      node = nextElement(node);
     }
   }
   return [node, offset]
@@ -40,14 +52,15 @@ function recordSelection() {
   var new_node = document.createElement('p');
   if(current_selection === null) {
     new_node.innerHTML = (
-      '<a href="javascript:restoreSelection(null);">empty</a>'
+      'empty <a href="javascript:restoreSelection(null);">[restore]</a>'
     );
   } else {
+    var repr = '[\'' + current_selection[0] + '\', \'' + current_selection[1] + '\']';
     new_node.innerHTML = (
-      '<a href="javascript:restoreSelection(' +
-      '[\'' + current_selection[0] + '\', \'' + current_selection[1] + '\']);">' +
       current_selection[0] + ' ; ' + current_selection[1] +
-      '</a>'
+      ' <a href="javascript:restoreSelection(' +
+      repr + ');">[restore]</a>' +
+      ' <a href="javascript:highlightSelection(' + repr + ');">[highlight]</a>'
     );
   }
   output.parentNode.insertBefore(new_node, output);
@@ -81,17 +94,58 @@ function selectionChanged() {
 document.addEventListener('selectionchange', selectionChanged);
 
 function restoreSelection(saved) {
-  console.log("TODO restore:" + saved);
   var sel = window.getSelection();
   sel.removeAllRanges();
   if(saved !== null) {
     var range = new Range();//document.createRange();
     var start = locatePos(saved[0]);
-    console.log("set to start:", start);
     var end = locatePos(saved[1]);
-    console.log("set to end:", end);
     range.setStart(start[0], start[1]);
     range.setEnd(end[0], end[1]);
     sel.addRange(range);
   }
+}
+
+function splitAtPos(pos, after) {
+  if(pos[1] == 0) {
+    return pos[0];
+  } else if(pos[1] == pos[0].textContent.length) {
+    return nextElement(pos[0]);
+  } else {
+    return pos[0].splitText(pos[1]);
+  }
+}
+
+function highlightSelection(saved) {
+  if(saved == null) {
+    return;
+  }
+  var start = locatePos(saved[0]);
+  start = splitAtPos(start, false);
+  var end = locatePos(saved[1]);
+  end = splitAtPos(end, true);
+
+  var node = start;
+  while(node != end) {
+    var next = nextElement(node);
+    if(node.nodeType == 3) {
+      var span = document.createElement('span');
+      span.className = 'highlight';
+      node.parentNode.insertBefore(span, node);
+      span.appendChild(node);
+    }
+    node = next;
+  }
+}
+
+function getPageXY(e) {
+  // from jQuery
+  // Calculate pageX/Y if missing
+  if(e.pageX == null) {
+    var doc = document.documentElement, body = document.body;
+    var x = e.clientX + (doc && doc.scrollLeft || body && body.scrollLeft || 0) - (doc.clientLeft || 0);
+    var y = e.clientY + (doc && doc.scrollTop || body && body.scrollTop || 0) - (doc.clientTop || 0);
+    return {x: x, y: y};
+  }
+  return {x: e.pageX, y: e.pageY};
 }
