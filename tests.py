c7e860bdb64f7b333029b434b6bd8af0698bf9b7
Remi Rampin <remirampin@gmail.com>
2018-09-18 20:19:52 -0400

    Fix test for asynchronous conversion

--- tests.py
+++ tests.py
@@ -1,15 +1,17 @@
+from tornado.testing import AsyncTestCase, gen_test
 import unittest
 
 from tagger import convert
 
 
-class TestConvert(unittest.TestCase):
-    def test_convert_txt(self):
+class TestConvert(AsyncTestCase):
+    @gen_test
+    async def test_convert_txt(self):
         body = (
             b"\n  This is an example text document.\nIt should be converted. "
             b"\n\n  It has another paragraph here.\n  "
         )
-        body = convert.to_numbered_html(body, 'text/plain', 'test.txt')
+        body = await convert.to_numbered_html(body, 'text/plain', 'test.txt')
         self.assertEqual(
             body,
             "<p id=\"doc-item-1\">This is an example text document.\nIt should be converted.</p>\n\n"
