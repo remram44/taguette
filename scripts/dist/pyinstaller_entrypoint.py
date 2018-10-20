7057960b503fcade9c711b9aef242085ed765625
Remi Rampin <remirampin@gmail.com>
2018-10-19 18:58:07 -0700

    Find Calibre in 'Calibre2' subfolder

--- scripts/dist/pyinstaller_entrypoint.py
+++ scripts/dist/pyinstaller_entrypoint.py
@@ -1,4 +1,13 @@
+import os
+import sys
+
 from taguette.web import main
 
+
+if sys.platform == 'win32':
+    os.environ['PATH'] = os.path.join(
+        os.path.dirname(sys.executable),
+        'Calibre2',
+    ) + os.pathsep + os.environ['PATH']
 
 main()
