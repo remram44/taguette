9d0ef158f98c89cbf3b58974427f8228c06856a2
Remi Rampin <remirampin@gmail.com>
2018-11-29 18:31:57 -0500

    Fix typo

--- taguette/convert.py
+++ taguette/convert.py
@@ -62,8 +62,8 @@ async def to_html(body, content_type, filename):
             logger.info("ebook-convert successful")
             output_filename = os.path.join(output_dir, 'index.html')
             if not os.path.isfile(output_filename):
-                raise RuntimeError("index.html not found in output dir: %r",
-                                   os.listdir(output_dir))
+                raise RuntimeError("index.html not found in output dir: %r" %
+                                   (os.listdir(output_dir),))
             with open(output_filename, 'rb') as fp:
                 body = fp.read()
             # TODO: Store media files
