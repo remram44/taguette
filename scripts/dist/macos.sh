21f7119c7b3cdfcebeac14b653cd6c141f0cdade
Remi Rampin <remirampin@gmail.com>
2018-12-13 02:21:42 +0000

    Add background to Mac DMG

--- scripts/dist/macos.sh
+++ scripts/dist/macos.sh
@@ -18,8 +18,11 @@ cp scripts/dist/macos/taguette_console_wrapper dist/Taguette.app/Contents/MacOS/
 cp -a /Applications/calibre.app dist/Taguette.app/Contents/Resources/
 (cd dist/Taguette.app/Contents && patch -p0 <../../../scripts/dist/macos/plist.patch)
 codesign --deep -s Taguette dist/Taguette.app
-rm -f taguette.dmg
 cp scripts/dist/LICENSE.txt dist/LICENSE.txt
 ln -s /Applications dist/Applications
 sleep 1
-hdiutil create taguette.dmg -srcfolder dist -volname "Taguette $VERSION"
+#hdiutil create taguette.dmg -srcfolder dist -volname "Taguette $VERSION"
+EMPTYDIR="$(mktemp -d /tmp/emptydir.XXXXXX)"
+rm -f taguette.dmg
+../create-dmg/create-dmg --volname "Taguette $VERSION" --icon-size 64 --background scripts/dist/macos/dmg-background.png --window-size 480 350 --add-file LICENSE.txt scripts/dist/LICENSE.txt 113 67 --app-drop-link 111 228 --add-folder Taguette.app dist/Taguette.app 355 220 taguette.dmg "$EMPTYDIR"
+rmdir "$EMPTYDIR"
