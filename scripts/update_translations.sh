a40cc82da830b99616afa0c18d9fee30db239383
Remi Rampin <r@remirampin.com>
2019-05-08 18:04:44 +0200

    CI: Build MO files

--- scripts/update_translations.sh
+++ scripts/update_translations.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
+set -eu
+
 cd "$(dirname "$(dirname "$0")")"
 pybabel extract -F scripts/babelrc -o po/taguette.pot $(find taguette -name '*.py' -o -name '*.js' -o -name '*.html')
-for i in po/*.mo; do
-    DIR=taguette/l10n/$(echo $i | sed 's/^po\///' | sed 's/\.mo$//')/LC_MESSAGES
-    mkdir -p $DIR
-    cp $i $DIR/taguette.mo
+find po -name '*.po' | while read fin; do
+    loc=$(echo $fin | sed 's/^po\///' | sed 's/\.po$//')
+    dir=taguette/l10n/$loc/LC_MESSAGES
+    mkdir -p $dir
+    pybabel compile -i $fin -o $dir/taguette.mo
 done
