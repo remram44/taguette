28d2786a0148a66598a3d1632e1f578f52e7efae
Remi Rampin <r@remirampin.com>
2019-06-11 20:01:53 -0400

    Fix missing _f() from POT files

--- scripts/update_pot.sh
+++ scripts/update_pot.sh
@@ -3,5 +3,5 @@
 set -eu
 
 cd "$(dirname "$(dirname "$0")")"
-pybabel extract -F scripts/babelrc -c TRANSLATORS: -s -o po/main.pot $(find taguette -name '*.py' -o -name '*.html')
+pybabel extract -F scripts/babelrc -k _f -c TRANSLATORS: -s -o po/main.pot $(find taguette -name '*.py' -o -name '*.html')
 pybabel extract -F scripts/babelrc -c TRANSLATORS: -s -o po/javascript.pot $(find taguette -name '*.js')
