5b5aed5c5c2929f95815859b405b04f0a55cfe10
Remi Rampin <r@remirampin.com>
2019-11-16 13:42:39 -0500

    Hide 'delete project' button if not allowed

--- taguette/web/views.py
+++ taguette/web/views.py
@@ -454,19 +454,27 @@ class Project(BaseHandler):
             self.db.query(database.ProjectMember)
             .filter(database.ProjectMember.project_id == project_id)
         ).all()
+        can_delete_project = any(
+            member.user_login == self.current_user and
+            member.privileges.can_delete_project()
+            for member in members
+        )
         members_json = jinja2.Markup(json.dumps(
             {member.user_login: {'privileges': member.privileges.name}
              for member in members}
         ))
         _ = self.xsrf_token  # Make sure XSRF cookie is set
-        return self.render('project.html',
-                           project=project,
-                           last_event=(project.last_event
-                                       if project.last_event is not None
-                                       else -1),
-                           documents=documents_json,
-                           user_login=jinja2.Markup(
-                               json.dumps(self.current_user)
-                           ),
-                           tags=tags_json,
-                           members=members_json)
+        return self.render(
+            'project.html',
+            project=project,
+            last_event=(project.last_event
+                        if project.last_event is not None
+                        else -1),
+            documents=documents_json,
+            user_login=jinja2.Markup(
+                json.dumps(self.current_user)
+            ),
+            tags=tags_json,
+            members=members_json,
+            can_delete_project=can_delete_project,
+        )
