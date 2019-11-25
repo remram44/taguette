bc7bb5f45de729abb064c86319d839e732e5c2da
Remi Rampin <r@remirampin.com>
2019-11-25 17:44:49 -0500

    Fix minor typo in HTML

--- taguette/static/js/taguette.js
+++ taguette/static/js/taguette.js
@@ -752,7 +752,7 @@ function updateTagsList() {
       '    <a id="tag-link-' + tag.id + '">' + escapeHtml(tag.path) + '</a>' +
       '  </div>' +
       '  <div style="white-space: nowrap;">' +
-      '    <span href="#" class="badge badge-secondary badge-pill" id="tag-' + tag.id + '-count">' + tag.count + '</span>' +
+      '    <span class="badge badge-secondary badge-pill" id="tag-' + tag.id + '-count">' + tag.count + '</span>' +
       '    <a href="javascript:editTag(' + tag.id + ');" class="badge badge-primary badge-pill">' + gettext("edit") + '</a>' +
       '  </div>' +
       '</div>' +
