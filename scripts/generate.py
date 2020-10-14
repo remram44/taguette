5effd760a9a05021f3a03240740b5725f6f44818
Remi Rampin <r@remirampin.com>
2020-10-13 22:48:40 -0400

    Update generate.py to render all languages

--- scripts/generate.py
+++ scripts/generate.py
@@ -10,21 +10,21 @@ import sys
 logger = logging.getLogger('generate')
 
 
-trans = gettext.NullTranslations()
+class TranslationWrapper(object):
+    def __init__(self, trans):
+        self._trans = trans
 
+    def gettext(self, message, **kwargs):
+        message = self._trans.gettext(message)
+        if kwargs:
+            message = message % kwargs
+        return message
 
-def gettext(message, **kwargs):
-    message = trans.gettext(message)
-    if kwargs:
-        message = message % kwargs
-    return message
-
-
-def ngettext(singular, plural, n, **kwargs):
-    message = trans.ngettext(singular, plural, n)
-    if kwargs:
-        message = message % kwargs
-    return message
+    def ngettext(self, singular, plural, n, **kwargs):
+        message = self._trans.ngettext(singular, plural, n)
+        if kwargs:
+            message = message % kwargs
+        return message
 
 
 def main():
@@ -32,22 +32,54 @@ def main():
 
     out = sys.argv[1]
 
+    # Initialize templates
     template_env = jinja2.Environment(
         loader=jinja2.FileSystemLoader(['.']),
         autoescape=jinja2.select_autoescape(['html']),
         extensions=['jinja2.ext.i18n'],
     )
 
-    for page in glob.glob('*.html'):
-        if page.startswith('_'):
-            continue
-        logger.info("Rendering %s...", page)
-        template = template_env.get_template(page)
-        with open(os.path.join(out, page), 'w') as f_out:
-            f_out.write(template.render(
-                gettext=gettext,
-                ngettext=ngettext,
-            ))
+    # List languages
+    languages = [None]
+    for locale in os.listdir('l10n'):
+        if os.path.exists(os.path.join(
+            'l10n', locale,
+            'LC_MESSAGES', 'taguette_website.mo',
+        )):
+            languages.append(locale)
+
+    for language in languages:
+        logger.info(
+            "Language: %s",
+            language if language is not None else "(original English)",
+        )
+
+        if language is None:
+            out_dir = out
+
+            trans = gettext.NullTranslations()
+        else:
+            out_dir = os.path.join(out, language.split('_', 1)[0])
+            try:
+                os.mkdir(out_dir)
+            except FileExistsError:
+                pass
+
+            trans = gettext.translation('taguette_website', 'l10n', [language])
+
+        trans = TranslationWrapper(trans)
+
+        # Render pages
+        for page in glob.glob('*.html'):
+            if page.startswith('_'):
+                continue
+            logger.info("Rendering %s...", page)
+            template = template_env.get_template(page)
+            with open(os.path.join(out_dir, page), 'w') as f_out:
+                f_out.write(template.render(
+                    gettext=trans.gettext,
+                    ngettext=trans.ngettext,
+                ))
 
 
 if __name__ == '__main__':
