946250dcb8d182db10d1729aef39aa5c62d4fbbf
Remi Rampin <remi@rampin.org>
2022-03-09 14:03:19 -0500

    Update routing for subdirectory installations

--- taguette/web/__init__.py
+++ taguette/web/__init__.py
@@ -62,91 +62,118 @@ class Error404(BaseHandler):
         raise HTTPError(404)
 
 
+class UnbakedURLSpec(object):
+    def __init__(self, pattern, handler, kwargs=None, name=None):
+        self.pattern = pattern
+        self.handler = handler
+        self.kwargs = kwargs
+        self.name = name
+
+    def bake(self, *, base_path):
+        return URLSpec(
+            base_path + self.pattern,
+            self.handler,
+            kwargs=self.kwargs,
+            name=self.name,
+        )
+
+
 def make_app(config, debug=False, xsrf_cookies=True):
+    routes = [
+        # Basic pages
+        UnbakedURLSpec('/', views.Index, name='index'),
+        UnbakedURLSpec('/cookies', views.CookiesPrompt, name='cookies_prompt'),
+        UnbakedURLSpec('/login', views.Login, name='login'),
+        UnbakedURLSpec('/logout', views.Logout, name='logout'),
+        UnbakedURLSpec('/register', views.Register, name='register'),
+        UnbakedURLSpec('/tos', views.TermsOfService, name='tos'),
+        UnbakedURLSpec('/account', views.Account, name='account'),
+        UnbakedURLSpec('/reset_password', views.AskResetPassword,
+                       name='reset_password'),
+        UnbakedURLSpec('/new_password', views.SetNewPassword,
+                       name='new_password'),
+        UnbakedURLSpec('/project/new', views.ProjectAdd, name='new_project'),
+        UnbakedURLSpec('/project/import', views.ProjectImport,
+                       name='import_project'),
+        UnbakedURLSpec('/project/([0-9]+)/delete', views.ProjectDelete,
+                       name='delete_project'),
+        UnbakedURLSpec('/project/([0-9]+)/import_codebook',
+                       views.ImportCodebook,
+                       name='import_codebook'),
+
+        # Project view
+        UnbakedURLSpec('/project/([0-9]+)', views.Project, name='project'),
+        UnbakedURLSpec('/project/([0-9]+)/document/[0-9]+', views.Project,
+                       name='project_doc'),
+        UnbakedURLSpec('/project/([0-9]+)/highlights/.*', views.Project,
+                       name='project_tag'),
+
+        # Export options
+        UnbakedURLSpec('/project/([0-9]+)/export/project\\.sqlite3',
+                       export.ExportSqlite, name='export_project_sqlite'),
+        UnbakedURLSpec('/project/([0-9]+)/export/codebook\\.qdc',
+                       export.ExportCodebookXml, name='export_codebook_qdc'),
+        UnbakedURLSpec('/project/([0-9]+)/export/codebook\\.csv',
+                       export.ExportCodebookCsv, name='export_codebook_csv'),
+        UnbakedURLSpec('/project/([0-9]+)/export/codebook\\.xlsx',
+                       export.ExportCodebookXlsx, name='export_codebook_xlsx'),
+        UnbakedURLSpec('/project/([0-9]+)/export/codebook\\.([a-z0-3]{2,4})',
+                       export.ExportCodebookDoc, name='export_codebook_doc'),
+        UnbakedURLSpec('/project/([0-9]+)/export/document/'
+                       '([^/]+)\\.([a-z0-9]{2,4})',
+                       export.ExportDocument, name='export_document'),
+        UnbakedURLSpec('/project/([0-9]+)/export/highlights/(.*)\\.csv',
+                       export.ExportHighlightsCsv),
+        UnbakedURLSpec('/project/([0-9]+)/export/highlights/(.*)\\.xlsx',
+                       export.ExportHighlightsXlsx),
+        UnbakedURLSpec('/project/([0-9]+)/export/highlights/'
+                       '(.*)\\.([a-z0-3]{2,4})',
+                       export.ExportHighlightsDoc,
+                       name='export_highlights_doc'),
+
+        # API
+        UnbakedURLSpec('/api/check_user', api.CheckUser),
+        UnbakedURLSpec('/api/import', api.ProjectImport),
+        UnbakedURLSpec('/api/project/([0-9]+)', api.ProjectMeta),
+        UnbakedURLSpec('/api/project/([0-9]+)/document/new', api.DocumentAdd),
+        UnbakedURLSpec('/api/project/([0-9]+)/document/([0-9]+)',
+                       api.Document),
+        UnbakedURLSpec('/api/project/([0-9]+)/document/([0-9]+)/contents',
+                       api.DocumentContents),
+        UnbakedURLSpec('/api/project/([0-9]+)/document/([0-9]+)/highlight/new',
+                       api.HighlightAdd),
+        UnbakedURLSpec(
+            '/api/project/([0-9]+)/document/([0-9]+)/highlight/([0-9]+)',
+            api.HighlightUpdate),
+        UnbakedURLSpec('/api/project/([0-9]+)/highlights/(.*)',
+                       api.Highlights),
+        UnbakedURLSpec('/api/project/([0-9]+)/tag/new', api.TagAdd),
+        UnbakedURLSpec('/api/project/([0-9]+)/tag/([0-9]+)', api.TagUpdate),
+        UnbakedURLSpec('/api/project/([0-9]+)/tag/merge', api.TagMerge),
+        UnbakedURLSpec('/api/project/([0-9]+)/members', api.MembersUpdate),
+        UnbakedURLSpec('/api/project/([0-9]+)/events', api.ProjectEvents),
+
+        # Translation catalog and functions
+        UnbakedURLSpec('/trans\\.js', TranslationJs, name='trans.js'),
+
+        # Messages
+        URLSpec('/messages\\.js', MessagesJs, name='messages.js'),
+
+        # Well-known URLs
+        URLSpec('/\\.well-known/change-password', RedirectAccount),
+    ]
+
+    base_path = config['BASE_PATH']
+    routes = [
+        spec.bake(base_path=base_path) if isinstance(spec, UnbakedURLSpec)
+        else spec
+        for spec in routes
+    ]
+
     return Application(
-        [
-            # Basic pages
-            URLSpec('/', views.Index, name='index'),
-            URLSpec('/cookies', views.CookiesPrompt, name='cookies_prompt'),
-            URLSpec('/login', views.Login, name='login'),
-            URLSpec('/logout', views.Logout, name='logout'),
-            URLSpec('/register', views.Register, name='register'),
-            URLSpec('/tos', views.TermsOfService, name='tos'),
-            URLSpec('/account', views.Account, name='account'),
-            URLSpec('/reset_password', views.AskResetPassword,
-                    name='reset_password'),
-            URLSpec('/new_password', views.SetNewPassword,
-                    name='new_password'),
-            URLSpec('/project/new', views.ProjectAdd, name='new_project'),
-            URLSpec('/project/import', views.ProjectImport,
-                    name='import_project'),
-            URLSpec('/project/([0-9]+)/delete', views.ProjectDelete,
-                    name='delete_project'),
-            URLSpec('/project/([0-9]+)/import_codebook', views.ImportCodebook,
-                    name='import_codebook'),
-
-            # Project view
-            URLSpec('/project/([0-9]+)', views.Project, name='project'),
-            URLSpec('/project/([0-9]+)/document/[0-9]+', views.Project,
-                    name='project_doc'),
-            URLSpec('/project/([0-9]+)/highlights/.*', views.Project,
-                    name='project_tag'),
-
-            # Export options
-            URLSpec('/project/([0-9]+)/export/project\\.sqlite3',
-                    export.ExportSqlite, name='export_project_sqlite'),
-            URLSpec('/project/([0-9]+)/export/codebook\\.qdc',
-                    export.ExportCodebookXml, name='export_codebook_qdc'),
-            URLSpec('/project/([0-9]+)/export/codebook\\.csv',
-                    export.ExportCodebookCsv, name='export_codebook_csv'),
-            URLSpec('/project/([0-9]+)/export/codebook\\.xlsx',
-                    export.ExportCodebookXlsx, name='export_codebook_xlsx'),
-            URLSpec('/project/([0-9]+)/export/codebook\\.([a-z0-3]{2,4})',
-                    export.ExportCodebookDoc, name='export_codebook_doc'),
-            URLSpec('/project/([0-9]+)/export/document/'
-                    '([^/]+)\\.([a-z0-9]{2,4})',
-                    export.ExportDocument, name='export_document'),
-            URLSpec('/project/([0-9]+)/export/highlights/(.*)\\.csv',
-                    export.ExportHighlightsCsv),
-            URLSpec('/project/([0-9]+)/export/highlights/(.*)\\.xlsx',
-                    export.ExportHighlightsXlsx),
-            URLSpec('/project/([0-9]+)/export/highlights/'
-                    '(.*)\\.([a-z0-3]{2,4})',
-                    export.ExportHighlightsDoc, name='export_highlights_doc'),
-
-            # API
-            URLSpec('/api/check_user', api.CheckUser),
-            URLSpec('/api/import', api.ProjectImport),
-            URLSpec('/api/project/([0-9]+)', api.ProjectMeta),
-            URLSpec('/api/project/([0-9]+)/document/new', api.DocumentAdd),
-            URLSpec('/api/project/([0-9]+)/document/([0-9]+)',
-                    api.Document),
-            URLSpec('/api/project/([0-9]+)/document/([0-9]+)/contents',
-                    api.DocumentContents),
-            URLSpec('/api/project/([0-9]+)/document/([0-9]+)/highlight/new',
-                    api.HighlightAdd),
-            URLSpec(
-                '/api/project/([0-9]+)/document/([0-9]+)/highlight/([0-9]+)',
-                api.HighlightUpdate),
-            URLSpec('/api/project/([0-9]+)/highlights/(.*)',
-                    api.Highlights),
-            URLSpec('/api/project/([0-9]+)/tag/new', api.TagAdd),
-            URLSpec('/api/project/([0-9]+)/tag/([0-9]+)', api.TagUpdate),
-            URLSpec('/api/project/([0-9]+)/tag/merge', api.TagMerge),
-            URLSpec('/api/project/([0-9]+)/members', api.MembersUpdate),
-            URLSpec('/api/project/([0-9]+)/events', api.ProjectEvents),
-
-            # Translation catalog and functions
-            URLSpec('/trans\\.js', TranslationJs, name='trans.js'),
-
-            # Messages
-            URLSpec('/messages\\.js', MessagesJs, name='messages.js'),
-
-            # Well-known URLs
-            URLSpec('/\\.well-known/change-password', RedirectAccount),
-        ],
+        routes,
         static_path=pkg_resources.resource_filename('taguette', 'static'),
-        login_url='/login',
+        login_url=base_path + '/login',
         xsrf_cookies=xsrf_cookies,
         debug=debug,
         config=config,
