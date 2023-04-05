078166c36ca98585f75971120fac52551089dbab
Remi Rampin <remi@rampin.org>
2023-04-05 15:53:33 -0400

    Make sure not to include .orig file in MacOS app

--- scripts/dist/macos.sh
+++ scripts/dist/macos.sh
@@ -17,7 +17,7 @@ pyinstaller macos.spec
 rm -rf dist/taguette
 cp scripts/dist/macos/taguette_console_wrapper dist/Taguette.app/Contents/MacOS/
 cp -a /Applications/calibre.app dist/Taguette.app/Contents/Resources/
-(cd dist/Taguette.app/Contents && patch -p0 <../../../scripts/dist/macos/plist.patch)
+(cd dist/Taguette.app/Contents && patch -p0 <../../../scripts/dist/macos/plist.patch && rm -f Info.plist.orig)
 codesign --deep --force -s Taguette dist/Taguette.app
 rm -f taguette.dmg
 cp scripts/dist/LICENSE.txt dist/LICENSE.txt
