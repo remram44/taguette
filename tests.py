157111c5c10d51e86f8958e3b2190813df992000
Philippe Boissonneault <philippe.boissonneault@usherbrooke.ca>
2023-12-13 09:16:41 -0500

    test timeout pour cicd - test_export

--- tests.py
+++ tests.py
@@ -1611,7 +1611,7 @@ class TestMultiuser(MyHTTPTestCase):
         ])
 
     @with_tempdir
-    @gen_test(timeout=120)
+    @gen_test(timeout=15)
     async def test_import(self, tmp):
         # Populate database
         db1 = self.application.DBSession()
@@ -1797,7 +1797,7 @@ class TestMultiuser(MyHTTPTestCase):
         )
 
     @with_tempdir
-    @gen_test
+    @gen_test(timeout=15)
     async def test_export(self, tmp):
         # Populate database
         db1 = self.application.DBSession()
